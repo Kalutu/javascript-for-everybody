e845b376363e0064cfdb732836abf98ab01615c5
Kalutu Daniel <kalutudaniel@gmail.com>
2023-05-12 22:31:58 +0300

    Update reverseString.js

--- Reverse a String/reverseString.js
+++ Reverse a String/reverseString.js
@@ -1,7 +1,6 @@
-function reverseString(str){
-    return str.split("").reverse().join();
+function reverseString(str) {
+  return str
+    .split("")
+    .reverse()
+    .join("");
 }
-
-let inputString = "Hello";
-let reversed = reverseString(inputString);
-console.log(reversed);
