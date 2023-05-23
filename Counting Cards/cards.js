5e24a2e85d4b53f1700200b3ce8a552bc988afb0
Kalutu Daniel <kalutudaniel@gmail.com>
2023-05-23 14:17:10 +0300

    Update cards.js

--- Counting Cards/cards.js
+++ Counting Cards/cards.js
@@ -1,1 +1,31 @@
+let count = 0;
 
+function cc(card) {
+  switch(card)
+  {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+          count++;
+          break;
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+            count--;
+            break;
+  }
+  if(count>0)
+  {
+    return count+" Bet";
+  }
+  else
+  {
+    return count+" Hold"
+  }
+}
+
+console.log(cc('A')); cc(3); cc(7); cc('K'); cc('A');
