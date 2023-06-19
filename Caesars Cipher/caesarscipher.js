b94fa9711a02c1b82430001b63f055bca44bc041
Kalutu Daniel <kalutudaniel@gmail.com>
2023-06-19 22:08:17 +0300

    Update caesarscipher.js

--- Caesars Cipher/caesarscipher.js
+++ Caesars Cipher/caesarscipher.js
@@ -1,1 +1,23 @@
-
+function rot13(str) {
+  // Split str into a character array
+  return (
+    str
+      .split("")
+      // Iterate over each character in the array
+      .map.call(str, function(char) {
+        // Convert char to a character code
+        var x = char.charCodeAt(0);
+        // Checks if character lies between A-Z
+        if (x < 65 || x > 90) {
+          return String.fromCharCode(x); // Return un-converted character
+        }
+        //N = ASCII 78, if the character code is less than 78, shift forward 13 places
+        else if (x < 78) {
+          return String.fromCharCode(x + 13);
+        }
+        // Otherwise shift the character 13 places backward
+        return String.fromCharCode(x - 13);
+      })
+      .join("")
+  ); // Rejoin the array into a string
+}
