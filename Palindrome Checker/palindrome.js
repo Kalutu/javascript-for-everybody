13c517f035695dbd0078003b5198a644b3931ca0
Kalutu Daniel <kalutudaniel@gmail.com>
2023-06-19 21:39:51 +0300

    Update palindrome.js

--- Palindrome Checker/palindrome.js
+++ Palindrome Checker/palindrome.js
@@ -1,1 +1,8 @@
+function isPalindrome(s) {
+  // Remove non-alphanumeric characters and convert to lowercase
+  s = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+  // Check if the string is equal to its reverse
+  return s === s.split('').reverse().join('');
+}
 
