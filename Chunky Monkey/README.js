ae397efd1bcd4496f669341f7a380357a0939181
Kalutu Daniel <kalutudaniel@gmail.com>
2023-06-23 22:29:49 +0300

    Update README.js

--- Chunky Monkey/README.js
+++ Chunky Monkey/README.js
@@ -1,1 +1,11 @@
+# chunkArrayInGroups
+A JavaScript function that splits an array into groups of a specified size.
+
+## Usage
+```javascript
+const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+const size = 3;
+const result = chunkArrayInGroups(arr, size);
+console.log(result);
+// Output: [[1, 2, 3], [4, 5, 6], [7, 8]]
 
