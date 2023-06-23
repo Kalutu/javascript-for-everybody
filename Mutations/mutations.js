67e0464c116877c7cc791e80fb0bb27b6df97ba6
Kalutu Daniel <kalutudaniel@gmail.com>
2023-06-23 22:39:43 +0300

    Update mutations.js

--- Mutations/mutations.js
+++ Mutations/mutations.js
@@ -1,1 +1,8 @@
-
+function mutation(arr) {
+  const test = arr[1].toLowerCase();
+  const target = arr[0].toLowerCase();
+  for (let i = 0; i < test.length; i++) {
+    if (target.indexOf(test[i]) < 0) return false;
+  }
+  return true;
+}
