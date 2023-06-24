cea1d5814d9611550cbd4f0cb76b23569096558a
Kalutu Daniel <kalutudaniel@gmail.com>
2023-06-24 15:46:56 +0300

    Update script.js

--- DNA Pairing/script.js
+++ DNA Pairing/script.js
@@ -1,1 +1,26 @@
+function pairElement(str) {
+  // Function to match each character with the base pair
+  const matchWithBasePair = function(char) {
+    switch (char) {
+      case "A":
+        return ["A", "T"];
+      case "T":
+        return ["T", "A"];
+      case "C":
+        return ["C", "G"];
+      case "G":
+        return ["G", "C"];
+    }
+  };
 
+  // Find pair for every character in the string
+  const pairs = [];
+  for (let i = 0; i < str.length; i++) {
+    pairs.push(matchWithBasePair(str[i]));
+  }
+
+  return pairs;
+}
+
+// test here
+pairElement("GCG");
