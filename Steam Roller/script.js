f980f132d0accec3a641d362842ca5adc23ff851
Kalutu Daniel <kalutudaniel@gmail.com>
2023-06-25 22:19:12 +0300

    Update script.js

--- Steam Roller/script.js
+++ Steam Roller/script.js
@@ -1,1 +1,18 @@
+function steamrollArray(arr) {
+  const flattenedArray = [];
+  // Loop over array contents
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      // Recursively flatten entries that are arrays
+      //  and push into the flattenedArray
+      flattenedArray.push(...steamrollArray(arr[i]));
+    } else {
+      // Copy contents that are not arrays
+      flattenedArray.push(arr[i]);
+    }
+  }
+  return flattenedArray;
+};
 
+// test here
+steamrollArray([1, [2], [3, [[4]]]]);
